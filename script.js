aa1e661e699a41683bd114dde3a00546dc9ee2eb
HasiniMooli <iit2024094@iiita.ac.in>
2025-02-04 16:39:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let script=document.querySelector('h1');
+script.textContent="This is a Heading";
+let scrip=document.querySelector('p');
+scrip.textContent="This is a Paragraph";
+let anchor=document.querySelector('a');
+anchor.innerText="anchor tag";
+anchor.href="#";
